d15e36d5c9c38af8ca8c87d84faaf4aad544fe2e
manuligit <riina.vaisanen@gmail.com>
2018-01-25 13:15:35 +0200

    t1.1

--- src/index.js
+++ src/index.js
@@ -1,6 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+
+const Otsikko = (props) => {
+    return (
+        <h1>{props.kurssi}</h1>
+    )
+}
+
+const Sisalto = (props) => {
+    return (
+        <div>
+            <p>{props.osa1} {props.tehtavia1}</p>
+            <p>{props.osa2} {props.tehtavia2}</p>
+            <p>{props.osa3} {props.tehtavia3}</p>
+        </div>
+    )
+}
+
+const Yhteensa = (props) => {
+    return (
+        <p>yhteensä {props.tehtavia1 + props.tehtavia2 + props.tehtavia3} tehtävää</p>
+    )
+}
+
 const App = () => {
     const kurssi = 'Half Stack -sovelluskehitys'
     const osa1 = 'Reactin perusteet'
@@ -10,17 +33,13 @@ const App = () => {
     const osa3 = 'Komponenttien tila'
     const tehtavia3 = 14
 
-
 return (
     <div>
-        <h1>{kurssi}</h1>
-        <p>{osa1} {tehtavia1}</p>
-        <p>{osa2} {tehtavia2}</p>
-        <p>{osa3} {tehtavia3}</p>
-        <p>yhteensä {tehtavia1 + tehtavia2 + tehtavia3} tehtävää</p>
+        <Otsikko kurssi={kurssi} />
+        <Sisalto osa1={osa1} osa2={osa2} osa3={osa3} tehtavia1={tehtavia1} tehtavia2={tehtavia2} tehtavia3={tehtavia3} />
+        <Yhteensa tehtavia1={tehtavia1} tehtavia2={tehtavia2} tehtavia3={tehtavia3} />
     </div>
     )
 }
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+ReactDOM.render(<App />, document.getElementById('root'))
