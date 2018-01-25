7bd1b8f241083ab36c3b2afd8ce64d6a6cbbb6a1
manuligit <riina.vaisanen@gmail.com>
2018-01-25 13:19:28 +0200

    t1.2

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-
 const Otsikko = (props) => {
     return (
         <h1>{props.kurssi}</h1>
@@ -11,10 +10,16 @@ const Otsikko = (props) => {
 const Sisalto = (props) => {
     return (
         <div>
-            <p>{props.osa1} {props.tehtavia1}</p>
-            <p>{props.osa2} {props.tehtavia2}</p>
-            <p>{props.osa3} {props.tehtavia3}</p>
+            <Osa osa={props.osa1}  tehtavia={props.tehtavia1}/>
+            <Osa osa={props.osa2}  tehtavia={props.tehtavia2}/>
+            <Osa osa={props.osa3}  tehtavia={props.tehtavia3}/>
         </div>
+    )
+}
+
+const Osa = (props) => {
+    return (
+        <p>{props.osa} {props.tehtavia}</p>
     )
 }
 
